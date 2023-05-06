92da107a3dc08b1ad223bc238770aaaec6a400e6
Dani <djsimmonds@gmail.com>
2023-05-06 15:45:33 -0400

    datapack fixes load automatically

--- src/main/java/com/djs/tectonicbop/RegisterTectonicBiomes.java
+++ src/main/java/com/djs/tectonicbop/RegisterTectonicBiomes.java
@@ -9,21 +9,95 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-public class RegisterTectonicBOPBiomes {
+public class RegisterTectonicBiomes {
 
-	public static final DeferredRegister<Biome> REGISTER = DeferredRegister.create(ForgeRegistries.BIOMES, "tectonicbop");
-
-	public static final RegistryObject<Biome> SKYLANDS_AUTUMN = REGISTER.register("skylands_autumn", () -> {
-		return Biome.BiomeBuilder.from(OverworldBiomes.darkForest()).build();
+	public static final DeferredRegister<Biome> REGISTER = DeferredRegister.create(ForgeRegistries.BIOMES, "tectonic");
+	public static final RegistryObject<Biome> ALPINE_MEADOW = REGISTER.register("alpine_meadow", () -> {
+		return OverworldBiomes.theVoid();
 	});
-	public static final RegistryObject<Biome> SKYLANDS_WINTER = REGISTER.register("skylands_winter", () -> {
-		return Biome.BiomeBuilder.from(OverworldBiomes.darkForest()).build();
+	public static final RegistryObject<Biome> AUTUMN_BIRCH_FOREST = REGISTER.register("autumn_birch_forest", () -> {
+		return OverworldBiomes.theVoid();
 	});
-	public static final RegistryObject<Biome> SKYLANDS_SPRING = REGISTER.register("skylands_spring", () -> {
-		return Biome.BiomeBuilder.from(OverworldBiomes.darkForest()).build();
+	public static final RegistryObject<Biome> AUTUMN_FOREST = REGISTER.register("autumn_forest", () -> {
+		return OverworldBiomes.theVoid();
 	});
-	public static final RegistryObject<Biome> SKYLANDS_SUMMER = REGISTER.register("skylands_summer", () -> {
-		return Biome.BiomeBuilder.from(OverworldBiomes.darkForest()).build();
+	public static final RegistryObject<Biome> BADLANDS_SPIRES = REGISTER.register("badlands_spires", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> COLD_BEACH = REGISTER.register("cold_beach", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> COLD_CLIFFS = REGISTER.register("cold_cliffs", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> COLD_PLAINS = REGISTER.register("cold_plains", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> COLD_RIVER = REGISTER.register("cold_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> CORAL_RIVER = REGISTER.register("coral_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> DESERT_DUNES = REGISTER.register("desert_dunes", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> DRIPSTONE_RIVER = REGISTER.register("dripstone_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> EVERGREEN_FOREST = REGISTER.register("evergreen_forest", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> GRASSLANDS = REGISTER.register("grasslands", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> ICY_CLIFFS = REGISTER.register("icy_cliffs", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> ICY_RIVER = REGISTER.register("icy_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> ISLAND = REGISTER.register("island", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> LAGOON = REGISTER.register("lagoon", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> LANTERN_RIVER = REGISTER.register("lantern_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> LUKEWARM_BEACH = REGISTER.register("lukewarm_beach", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> LUKEWARM_CLIFFS = REGISTER.register("lukewarm_cliffs", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> LUKEWARM_RIVER = REGISTER.register("lukewarm_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> LUSH_RIVER = REGISTER.register("lush_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> OLD_GROWTH_EVERGREEN_FOREST = REGISTER.register("old_growth_evergreen_forest", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> OLD_GROWTH_SNOWY_TAIGA = REGISTER.register("old_growth_snowy_taiga", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> RED_DESERT = REGISTER.register("red_desert", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> RED_DESERT_DUNES = REGISTER.register("red_desert_dunes", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> SANDSTONE_CLIFFS = REGISTER.register("sandstone_cliffs", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> WARM_RIVER = REGISTER.register("warm_river", () -> {
+		return OverworldBiomes.theVoid();
+	});
+	public static final RegistryObject<Biome> WHITE_CLIFFS = REGISTER.register("white_cliffs", () -> {
+		return OverworldBiomes.theVoid();
 	});
 	
 //    @SubscribeEvent	
--- src/main/java/com/djs/tectonicbop/TectonicBOP.java
+++ src/main/java/com/djs/tectonicbop/TectonicBOP.java
@@ -1,64 +1,85 @@
 package com.djs.tectonicbop;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.minecraftforge.resource.PathResourcePack;
+import net.minecraftforge.event.AddPackFindersEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig.Type;
+import java.io.IOException;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(TectonicBOP.MOD_ID)
-public class TectonicBOP
-{
-    // Directly reference a slf4j logger
-//    private static final Logger LOGGER = LogUtils.getLogger();
-    public static final String MOD_ID = "tectonicbop";
-    
-    public TectonicBOP()
-    {
-        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-//        IEventBus bus = MinecraftForge.EVENT_BUS;
-        // Register the setup method for modloading
-//        modEventBus.addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-//        modEventBus.addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-//        modEventBus.addListener(this::processIMC);
+public class TectonicBOP {
+
+	public static final String MOD_ID = "tectonicbop";
+
+	public TectonicBOP() {
 		
-        // Register ourselves for server and other game events we are interested in
-//		modEventBus.addListener(this::gatherData);
-//        MinecraftForge.EVENT_BUS.register(this);
-                
-//        RegisterTectonicBOPBiomes.REGISTER.register(modEventBus);
-    }
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		ModLoadingContext.get().registerConfig(Type.COMMON, TectonicBOPConfig.GENERAL_SPEC, "tectonicbopconfig.toml");		
+		
+		modEventBus.addListener(this::loadTerratonicBuiltInDatapack);
+		modEventBus.addListener(this::setupBuiltInDatapack);
+		MinecraftForge.EVENT_BUS.register(this);
+		
+        RegisterTectonicBiomes.REGISTER.register(modEventBus);
+        RegisterTerralithBiomes.REGISTER.register(modEventBus);
+	}    
+
+	private void setupBuiltInDatapack(AddPackFindersEvent event) {	
+		if ((event.getPackType() == PackType.SERVER_DATA))
+		{
+			String BiomeLayout = TectonicBOPConfig.OVERWORLD_JSON.get().toString();
+		    String MODID = "tectonicbop";
+
+            var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("resourcepacks/" + BiomeLayout);
+            try (var pack = new PathResourcePack(ModList.get().getModFileById(MODID).getFile().getFileName() + ":" + resourcePath, resourcePath)) {
+				var metadataSection = pack.getMetadataSection(PackMetadataSection.SERIALIZER);
+				if (metadataSection != null)
+				{
+				    event.addRepositorySource((packConsumer, packConstructor) ->
+				            packConsumer.accept(packConstructor.create(
+				            		"builtin/" + BiomeLayout, new TextComponent("Tectonic BOP: " + BiomeLayout), true,
+				                    () -> pack, metadataSection, Pack.Position.TOP, PackSource.BUILT_IN, false)));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	// load terratonic in place of tectonic if bopTerralith is selected
+	// at this time, will need to remove tectonic mod for this to work, but will change once apollo integrates them
+	private void loadTerratonicBuiltInDatapack(AddPackFindersEvent event) {	
+		if ((event.getPackType() == PackType.SERVER_DATA) )
+		{
+			String BiomeLayout = TectonicBOPConfig.OVERWORLD_JSON.get().toString();
+			if (BiomeLayout.contains("Terratonic")) {
+			    String MODID = "tectonicbop"; // distribute in tectonicbop until apollo updates mod
+	
+	            var resourcePath = ModList.get().getModFileById(MODID).getFile().findResource("resourcepacks/terratonic");
+	            try (var pack = new PathResourcePack(ModList.get().getModFileById(MODID).getFile().getFileName() + ":" + resourcePath, resourcePath)) {
+					var metadataSection = pack.getMetadataSection(PackMetadataSection.SERIALIZER);
+					if (metadataSection != null)
+					{
+					    event.addRepositorySource((packConsumer, packConstructor) ->
+					            packConsumer.accept(packConstructor.create(
+					            		"builtin/terratonic", new TextComponent("Terratonic"), true,
+					                    () -> pack, metadataSection, Pack.Position.TOP, PackSource.BUILT_IN, false)));
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
 }
-//    private void setup(final FMLCommonSetupEvent event)
-//    {
-        // some preinit code
-//        LOGGER.info("HELLO FROM PREINIT");
-//        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-//    }
-
-//    private void enqueueIMC(final InterModEnqueueEvent event)
-//    {
-        // Some example code to dispatch IMC to another mod
-//        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-//    }
-
-//    private void processIMC(final InterModProcessEvent event)
-//    {
-        // Some example code to receive and process InterModComms from other mods
-//        LOGGER.info("Got IMC {}", event.getIMCStream().
-//                map(m->m.messageSupplier().get()).
-//                collect(Collectors.toList()));
-//    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-//    @SubscribeEvent
-//    public void onServerStarting(ServerStartingEvent event)
-//    {
-        // Do something when the server starts
-//        LOGGER.info("HELLO from server starting");
-//    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-//}
